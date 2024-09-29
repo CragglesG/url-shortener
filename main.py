acc583b24ed82ca8a27e84e4fa78cbc84492dbfa
CragglesG <121510295+CragglesG@users.noreply.github.com>
2024-09-29 14:37:39 +0100

    Fixed all bugs

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI, Response, HTTPException
+from typing import Annotated
+
+from fastapi import FastAPI, Response, HTTPException, Form
 from fastapi.responses import RedirectResponse, HTMLResponse
 import json
+
 app = FastAPI()
 
 with open("urls.json", "r") as f:
@@ -11,9 +14,6 @@ async def root():
     with open("index.html", "r") as file:
         return HTMLResponse(file.read())
 
-@app.get("/{tag}/{slug}")
-async def tag_slug(tag: str, slug: str):#
-    return RedirectResponse(urls[tag][slug])
 
 @app.get("/add/{slug}/{to}")
 async def add(slug: str, to: str):
@@ -21,15 +21,9 @@ async def add(slug: str, to: str):
     await _save()
     return Response()
 
-@app.get("/add/{tag}/{slug}/{to}")
-async def add(tag: str, slug: str, to: str):
-    urls[tag][slug] = to
-    await _save()
-    return Response()
-
-@app.post("/form/")
-async def form(slug: str, link: str):
-    await add(slug, link)
+@app.post("/form")
+async def form(slug: Annotated[str, Form()], link: Annotated[str, Form()]):
+    await add(slug=slug, to=link)
     with open("submitted.html", "r") as file:
         return HTMLResponse(file.read())
 @app.get("/_save")
@@ -37,9 +31,9 @@ async def _save():
     with open("urls.json", "w") as file:
         json.dump(urls, file, indent=2, sort_keys=True)
 
-@app.get("/{slug:path}")
+@app.get("/{slug}")
 async def slug_only(slug: str):
     if slug == "favicon.ico":
         return HTTPException(404)
     else:
-        return RedirectResponse(f"https://{urls["public"][slug]}")
+        return RedirectResponse(urls["public"][slug])
